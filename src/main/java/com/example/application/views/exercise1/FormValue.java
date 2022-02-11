416d0c3ed50e65f92083ee499af0780b617fc14a
nonWhistle <99025186+nonWhistle@users.noreply.github.com>
2022-02-11 13:22:40 +0000

    Added FormValue Class

---
+++ src/main/java/com/example/application/views/exercise1/FormValue.java
@@ -0,0 +1,32 @@
+package com.example.application.views.exercise1;
+
+public class FormValue
+{
+    String emailValue;
+    String stringValue;
+    String vaadinValue;
+
+    public String getEmailValue() {
+        return emailValue;
+    }
+
+    public void setEmailValue(String emailValue) {
+        this.emailValue = emailValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public String getVaadinValue() {
+        return vaadinValue;
+    }
+
+    public void setVaadinValue(String vaadinValue) {
+        this.vaadinValue = vaadinValue;
+    }
+}
